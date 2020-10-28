17eebfd5cbda48cf845cc689b4bbd3d4340a1aa4
James Monks <jameszmonks@gmail.com>
2020-10-28 19:41:24 +0000

    delete li working

--- BulletJournal.js
+++ BulletJournal.js
@@ -3,6 +3,8 @@ $(function(){
     init();
 });
 
+let tasklist = [];
+
 const _2days_ = 1000 * 60 * 60 * 24 * 2;
 
 console.log("anything blergh?");
@@ -12,27 +14,47 @@ function init()
     console.log("init");
     $("#bulletItem").on("submit", addBullet);
     //$("#submit").on("click", addBullet(event));
-}
 
+    //TODO: import tasks and set timer
+    //tasklist.push({id:id, timestamp:timestamp});
+}
 
 function addBullet(event) {
     console.log("addBullet");
     event.preventDefault();
 
-    let tf = $("#bullet");
-    let txt = tf.val();
-    tf.val("");
-    let bullet = document.createElement("li");
-    let chk = document.createElement("input");
+    let d = new Date();
+    let startTime = d.getTime();
 
+    let tf = $("#bullet");
+    let txt = "<SPAN class='memo'>" + tf.val() + "</SPAN>" + 
+              "<SPAN class='timestamp'>" + startTime + "</SPAN>";
+    tf.val("");
+
+    let bullet;
+    let li_id = "li_" + startTime;
+    if (tasklist.length % 2 == 0)
+        bullet = $("<li>").attr({ id: li_id, class: "zero" });
+    else
+        bullet = $("<li>").attr({ id: li_id, class: "one" });
+    let chk = document.createElement("input");
+    let btn = $("<input>").attr({ type: "button", value: "delete"});
+
+    btn.on("click", showMe);
     chk.addEventListener("click", showMe);
     chk["type"] = "checkbox";
     console.log(txt);
     console.log(chk);
-    bullet.appendChild(chk);
-    bullet.append(txt);
+    $(bullet).html(txt);
+    $(bullet).prepend(chk).append(btn);
     let bulletList = $("#bulletList");
     bulletList.append(bullet);
+
+    tasklist.push({ id: li_id, timeStamp: startTime });
+}
+
+function createList() {
+    //grab array, add each element individually
 }
 
 function showMe(event) {
@@ -42,7 +64,7 @@ function showMe(event) {
     console.log(d.getTime());
     let target = event.target;
     let liObject = $(target).parent();
-    //$(liObject).remove();
+    $(liObject).remove();
     console.log(target);
     console.log($(target).parent());
     //document.removeChild(parent);
